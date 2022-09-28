3cfbc13cca791e3a916038a71743eeec27e82893
yagofellipe <113136498+yagofellipe@users.noreply.github.com>
2022-09-28 14:00:42 -0300

    1

---
+++ Carrinho.py
@@ -0,0 +1,36 @@
+class CarrinhoDeCompras:
+    def __init__(self):
+      self.produtos = []
+
+    def inserir_produto(self, produto):
+        self.produtos.append(produto)
+    
+    def lista_produto(self):
+        for produto in self.produtos:
+            print(produto.nome, produto.valor)
+    def soma_total(self):
+        total = 0
+        for produto in self.produtos:
+            total += produto.valor
+        return total
+
+
+class Produto:
+    def __init__(self, nome, valor):
+      self.nome = nome
+      self.valor = valor
+
+class Serviço:
+    def __init__(self, nome, valor):
+      self.nome = nome
+      self.valor = valor
+
+carrinho = CarrinhoDeCompras()
+
+pi = Produto('roteador', 300)
+serv = Serviço('instalador',100)
+
+carrinho.inserir_produto(pi)
+carrinho.inserir_produto(serv)
+
+carrinho.lista_produto()
